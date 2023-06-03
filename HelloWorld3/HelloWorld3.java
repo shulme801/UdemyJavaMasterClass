4b7ce552925bf683a44c5c0f7d24f0b421f28d2c
Stephen H. Hulme <shulme801@gmail.com>
2023-06-03 15:59:39 -0400

    3 June last

--- HelloWorld3/HelloWorld3.java
+++ HelloWorld3/HelloWorld3.java
@@ -1,7 +1,7 @@
 public class HelloWorld3 {
     public static void main(String[] args) {
 
-        System.out.println("Hello, for the last time, Mr. Hulme");
+        System.out.println("Hello, for the last time, Mr.  Hulme");
         System.out.print("Goodbye, cruel world (and xcode CLT issues!!)");
 
     }
