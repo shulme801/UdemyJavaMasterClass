99bf71e642485bbd341fd02472275bff14089dc4
shulme801 <shulme801@gmail.com>
2023-06-04 15:19:58 -0400

    4 June Commit 3 Build the Code Test module

---
+++ CodeTest/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        if (isLeapYear(2000)) {
+            System.out.println("2000 is a leap year");
+        } else {
+            System.out.println("2000 is not a leap year");
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if ((year < 1) || (year > 9999)) {
+            return false;
+        } else {
+            return (year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0);
+        }
+    }
+}
+
