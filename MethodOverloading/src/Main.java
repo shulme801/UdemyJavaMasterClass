86085b1028bc97e87a8ab1ab93809dc3f485e7db
shulme801 <shulme801@gmail.com>
2023-06-25 13:50:24 -0400

    25 June Commit 3 of the MethodOverloading lesson

--- MethodOverloading/src/Main.java
+++ MethodOverloading/src/Main.java
@@ -2,10 +2,17 @@ public class Main {
     public static void main(String[] args) {
         int newScore = calculateScore("Stephen", 255);
         System.out.println("New score is " +  newScore);
+
+        newScore = calculateScore(75);
+        System.out.println("Unnamed player" + " scored " + newScore + " points");
     }
 
     public static int calculateScore(String playerName, int score) {
         System.out.println("Player " + playerName + " scored " + score + " points");
+        return(score * 1000);
+    }
+    public static int calculateScore(int score) {
+
         return(score * 1000);
     }
 }
