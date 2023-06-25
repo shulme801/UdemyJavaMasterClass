85cdc32f02b8f6666670b3a823a92d773ee43662
shulme801 <shulme801@gmail.com>
2023-06-25 15:12:42 -0400

    25 June Refactored convert to centimeters overloading challenge

--- OverloadingChallenge/src/Main.java
+++ OverloadingChallenge/src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int myInches = 2;
-        int myFeet   = 6;
+        int myInches = 8;
+        int myFeet   = 5;
+        int totalInches = (myFeet*12)+myInches;
 
-        System.out.println(myInches+
+        System.out.println(totalInches +
                 " inches is " +
-                convertToCentimeters(myInches) +
+                convertToCentimeters(totalInches) +
                 " centimeters");
 
         System.out.println(myFeet +
@@ -17,11 +18,15 @@ public class Main {
     }
 
 
-    public static double convertToCentimeters(int heightInInches) {
-        return heightInInches * 2.54;
+    public static double convertToCentimeters(int inches) {
+        return inches * 2.54;
     }
 
-    public static double convertToCentimeters(int heightInFeet, int heightInInches) {
-        return convertToCentimeters((heightInFeet*12) + heightInInches);
+    public static double convertToCentimeters(int feet, int inches) {
+        int feetToInches = feet * 12;
+        int totalInches = feetToInches + inches;
+
+        return convertToCentimeters(totalInches);
     }
+
 }
