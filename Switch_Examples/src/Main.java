369eaebab5e4d4fbfd9af18d5e4849c835d05866
shulme801 <shulme801@gmail.com>
2023-07-04 16:51:02 -0400

    4 July Switch Examples commit 3

--- Switch_Examples/src/Main.java
+++ Switch_Examples/src/Main.java
@@ -4,20 +4,30 @@ public class Main {
         int switchValue = 2;
 
         switch (switchValue) {
-            case 1:
-                System.out.println("Value was 1");
-                break;
-            case 2:
-                System.out.println("Value was 2");
-                break;
-            case 3: case 4: case 5:
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
                 System.out.println("Was a 3, a 4, or a 5");
                 System.out.println("Actually, it was a " + switchValue);
-                break;
-            default:
-                System.out.println("Was not 1, 2, 3, 4, or 5");
-                break;
+            }
+            default -> System.out.println("Was not 1, 2, 3, 4, or 5");
         }
-        System.out.println("All done!\n");
+
+        String month = "XYZ";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarter\n");
+    }
+
+    public static String getQuarter(String month) {
+
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> {
+                String badResponse = month + " is bad";
+                yield badResponse;
+            }
+        };
     }
 }
