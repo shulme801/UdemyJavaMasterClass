7abc7b757717931d13df450288eceec9b154e1ea
shulme801 <shulme801@gmail.com>
2023-07-04 16:21:42 -0400

    4 July Switch Examples commit 2

--- Switch_Examples/src/Main.java
+++ Switch_Examples/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        int switchValue = 3;
+        int switchValue = 2;
 
         switch (switchValue) {
             case 1:
@@ -10,8 +10,12 @@ public class Main {
             case 2:
                 System.out.println("Value was 2");
                 break;
+            case 3: case 4: case 5:
+                System.out.println("Was a 3, a 4, or a 5");
+                System.out.println("Actually, it was a " + switchValue);
+                break;
             default:
-                System.out.println("Value was neither 1 nor 2");
+                System.out.println("Was not 1, 2, 3, 4, or 5");
                 break;
         }
         System.out.println("All done!\n");
