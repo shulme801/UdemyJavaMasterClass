f7b84c7c273f3d08c32da6ba29aad5aec27a5422
shulme801 <shulme801@gmail.com>
2023-07-04 11:15:44 -0400

    4 July Minutes and seconds challenge commit 3

--- SecondsAndMinutesChallenge/src/Main.java
+++ SecondsAndMinutesChallenge/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(getDurationString(-9)); // This is the first test case
+
     }
 
     public static String getDurationString(int seconds) {
