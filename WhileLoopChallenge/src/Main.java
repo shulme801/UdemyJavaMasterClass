92462220508722632be92f9f59641b28c2eaa6bb
shulme801 <shulme801@gmail.com>
2023-07-13 10:58:49 -0400

    13 July While Loop Challenge Step 2

--- WhileLoopChallenge/src/Main.java
+++ WhileLoopChallenge/src/Main.java
@@ -2,17 +2,30 @@ public class Main {
     public static void main(String[] args) {
 
         int number = 4;
+        int startNumber = number + 1;
         int finishNumber = 20;
-        while (number <= finishNumber){
+        int oddCount = 0;
+        int evenCount = 0;
+
+        while (number <= finishNumber) {
             number++;
-            if (! isEvenNumber(number)) {
-                continue;
+            if (!isEvenNumber(number)) {
+                System.out.println(number + " is odd!");
+                oddCount++;
+            } else {
+                System.out.println(number + " is even!");
+                evenCount++;
             }
-            System.out.println(number + " is even!");
+
+            if (evenCount == 5) {
+                break;
+            }
         }
-
-
+        String range = startNumber + ":" + finishNumber;
+        System.out.println(("Count of odd numbers in range " + range + " is " + oddCount));
+        System.out.println(("Count of even numbers in range " + range + " is " + evenCount));
     }
+
 
     public static boolean isEvenNumber(int num) {
         return (num % 2 == 0);
